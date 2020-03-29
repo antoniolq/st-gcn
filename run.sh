2a8b1c243b5a308847715e8ab6dc368889d2c5d4
qing.liu <qing.liu@ihandysoft.com>
2020-03-29 22:25:05 +0800

    test

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python3.6.2 main.py demo_old --video resource/media/tumble.mp4 --openpose /home/qingl/antonio/openpose/build/ --./resource/results --device 3
+python3.6.2 main.py demo_old --video resource/media/tumble.mp4 --openpose /home/qingl/antonio/openpose/build/ --output_dir ./resource/results --device 3
 
