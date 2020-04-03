b97c47925488439abbdc18bfe23e7c64616d395a
qing.liu <qing.liu@ihandysoft.com>
2020-04-03 14:13:12 +0800

    test

--- net/st_gcn.py
+++ net/st_gcn.py
@@ -190,6 +190,8 @@ class GCN(nn.Module):
 
         res= self.residual(x)
         x, A = self.gcn(x, A)
+        print(type(x))
+        print(type(res))
         x = x + res
         return self.relu(x), A
 
