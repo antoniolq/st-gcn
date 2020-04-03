2e742815e2b08cdd13be88176162057b7cec7898
qing.liu <qing.liu@ihandysoft.com>
2020-04-03 12:15:08 +0800

    test

--- net/st_gcn.py
+++ net/st_gcn.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+import torchvision.models as models
+from torchsummary import summary
 
 from net.utils.tgcn import ConvTemporalGraphical
 from net.utils.graph import Graph
@@ -42,16 +44,26 @@ class Model(nn.Module):
         self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
         kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
         self.st_gcn_networks = nn.ModuleList((
-            st_gcn(in_channels, 64, kernel_size, 1, residual=False, **kwargs0),
-            st_gcn(64, 64, kernel_size, 1, **kwargs),
-            st_gcn(64, 64, kernel_size, 1, **kwargs),
-            st_gcn(64, 64, kernel_size, 1, **kwargs),
-            st_gcn(64, 128, kernel_size, 2, **kwargs),
-            st_gcn(128, 128, kernel_size, 1, **kwargs),
-            st_gcn(128, 128, kernel_size, 1, **kwargs),
-            st_gcn(128, 256, kernel_size, 2, **kwargs),
-            st_gcn(256, 256, kernel_size, 1, **kwargs),
-            st_gcn(256, 256, kernel_size, 1, **kwargs),
+            GCN(in_channels, 64, kernel_size, 1, residual=False, **kwargs0),
+            TCN(in_channels, 64, kernel_size, 1, residual=False, **kwargs0),
+            GCN(64, 64, kernel_size, 1, **kwargs),
+            TCN(64, 64, kernel_size, 1, **kwargs),
+            GCN(64, 64, kernel_size, 1, **kwargs),
+            TCN(64, 64, kernel_size, 1, **kwargs),
+            GCN(64, 64, kernel_size, 1, **kwargs),
+            TCN(64, 64, kernel_size, 1, **kwargs),
+            GCN(64, 128, kernel_size, 2, **kwargs),
+            TCN(64, 128, kernel_size, 2, **kwargs),
+            GCN(128, 128, kernel_size, 1, **kwargs),
+            TCN(128, 128, kernel_size, 1, **kwargs),
+            GCN(128, 128, kernel_size, 1, **kwargs),
+            TCN(128, 128, kernel_size, 1, **kwargs),
+            GCN(128, 256, kernel_size, 2, **kwargs),
+            TCN(128, 256, kernel_size, 2, **kwargs),
+            GCN(256, 256, kernel_size, 1, **kwargs),
+            TCN(256, 256, kernel_size, 1, **kwargs),
+            GCN(256, 256, kernel_size, 1, **kwargs),
+            TCN(256, 256, kernel_size, 1, **kwargs),
         ))
 
         # initialize parameters for edge importance weighting
@@ -115,7 +127,7 @@ class Model(nn.Module):
 
         return output, feature
 
-class st_gcn(nn.Module):
+class GCN(nn.Module):
     r"""Applies a spatial temporal graph convolution over an input graph sequence.
 
     Args:
@@ -155,6 +167,69 @@ class st_gcn(nn.Module):
 
         self.gcn = ConvTemporalGraphical(in_channels, out_channels,
                                          kernel_size[1])
+        if not residual:
+            self.residual = lambda x: 0
+
+        elif (in_channels == out_channels) and (stride == 1):
+            self.residual = lambda x: x
+
+        else:
+            self.residual = nn.Sequential(
+                nn.Conv2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=1,
+                    stride=(stride, 1)),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x, A):
+
+        res = self.residual(x)
+        x, A = self.gcn(x, A)
+        x += res
+
+        return self.relu(x), A
+
+class TCN(nn.Module):
+    r"""Applies a spatial temporal graph convolution over an input graph sequence.
+
+    Args:
+        in_channels (int): Number of channels in the input sequence data
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (tuple): Size of the temporal convolving kernel and graph convolving kernel
+        stride (int, optional): Stride of the temporal convolution. Default: 1
+        dropout (int, optional): Dropout rate of the final output. Default: 0
+        residual (bool, optional): If ``True``, applies a residual mechanism. Default: ``True``
+
+    Shape:
+        - Input[0]: Input graph sequence in :math:`(N, in_channels, T_{in}, V)` format
+        - Input[1]: Input graph adjacency matrix in :math:`(K, V, V)` format
+        - Output[0]: Outpu graph sequence in :math:`(N, out_channels, T_{out}, V)` format
+        - Output[1]: Graph adjacency matrix for output data in :math:`(K, V, V)` format
+
+        where
+            :math:`N` is a batch size,
+            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
+            :math:`T_{in}/T_{out}` is a length of input/output sequence,
+            :math:`V` is the number of graph nodes.
+
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dropout=0,
+                 residual=True):
+        super().__init__()
+
+        assert len(kernel_size) == 2
+        assert kernel_size[0] % 2 == 1
+        padding = ((kernel_size[0] - 1) // 2, 0)
 
         self.tcn = nn.Sequential(
             nn.BatchNorm2d(out_channels),
@@ -191,7 +266,6 @@ class st_gcn(nn.Module):
     def forward(self, x, A):
 
         res = self.residual(x)
-        x, A = self.gcn(x, A)
         x = self.tcn(x) + res
 
         return self.relu(x), A
